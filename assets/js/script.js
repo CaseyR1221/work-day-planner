dc8f18afd4e7140307798e46372741e75d1320a5
Casey R <crowlandsdev@gmail.com>
2021-07-01 23:42:16 -0400

    created preliminary js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,57 @@
-$(document).ready(function() {
+// Display today's day and date
+let currentDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(currentDate);
 
-let saveBtn = $(".saveBtn");
+$(document).ready(() => {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", () => {
+        // Get nearby values of the description in JQuery
+        let text = $(this).siblings(".description").val();
+        let time = $(this).parent().attr("id");
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+   
+    let timeTracker = () => {
+        //get current number of hours.
+        let currentTime = moment().hour();
+        console.log(currentTime);
 
-saveBtn.on("click", function() {
-    console.log("clicky clicky");
-})
+        // loop over time blocks
+        $(".time-block").each((index, element) => {
+            let blockTime = parseInt($(element).attr("id").split("hour-")[1]);
+            console.log(blockTime);
 
+            //To check the time and add the classes for background indicators
+            if (blockTime < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
 
+            }
+        })
+    }
 
+    // Get item from local storage if any
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    timeTracker();
 })
